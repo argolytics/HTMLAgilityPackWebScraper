8a33de6ecab79b7e36d0ea146f13003842d529a1
argolytics@pm.me <argolytics@pm.me>
2021-12-05 22:02:17 -0500

    Added additional error message handling

--- WebScraping/Program.cs
+++ WebScraping/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine("Validating in HtmlAgilityPack mode..." + Environment.NewLine);
 
 WebScraper scraper = new();
-PersonModel personResult = await scraper.ValidateLicense("658045");
+PersonModel personResult = await scraper.ValidateLicense("Amanda Williams", "658045");
 
 Console.WriteLine("Results:{0}---------{0}{1}", Environment.NewLine, personResult?.AsString() ?? "Error occurred");
 Console.ReadKey();
